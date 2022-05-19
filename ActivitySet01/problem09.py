33852beef6fade98d7e8b9a72df72e994c97bf8f
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-05-19 18:13:23 +0000

    list the file name

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,5 +1,10 @@
 # Lists
-
-while(1):
-  break
-  print("kaththe") 
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    for i in line.rstrip().split(' '):
+        lst.append(i)
+lst = list(set(lst))
+lst.sort()
+print(lst)
