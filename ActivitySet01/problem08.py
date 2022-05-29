62923613507b45dec04e0429068315df5a87e138
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-05-29 18:24:45 +0000

    files

--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,3 +1,13 @@
-wg= ["rgdg","xvc","vcb"] 
-for wgd in wg :
-   print("cvbb v", wgd)
+fname = input("Enter file name: ")
+fh = open(fname)
+ans = 0.0
+count = 0
+
+for line in fh:
+        if not line.startswith("X-DSPAM-Confidence:") :
+                continue
+        else:
+                ans = ans + float(line[20:])
+                count = count + 1
+
+print("Average spam confidence:", ans/count)
