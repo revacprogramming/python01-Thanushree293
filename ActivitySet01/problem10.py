d4981d6b23152f63051e45f7f7948f9ebcd6c355
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-06-05 14:52:54 +0000

    dictionary

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,15 +1,20 @@
 # Dictionaries
-fname = input("Enter file name: ")
-fh = open(fname)
-lst = list()
-for line in fh:
-    line = line.rstrip()
-    if line == " " : continue
-    words = line.split()
-    for i in words:
-        if i in lst:
-            continue
-        else:
-            lst.append(i)
-lst.sort()
-print(lst)
+name = raw_input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+counts = dict()
+handle = open(name)
+for line in handle:
+    line = line.strip()
+    if line.startswith('From ') :
+        person = line.split()
+        need = person[1]
+        counts[need] = counts.get(need,0)+1
+    else:
+        continue
+bigcount = None
+bigword = None
+for need,count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = need
+        bigcount = count  
+print(bigword, bigcount)
